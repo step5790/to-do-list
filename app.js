56906319539267a7737e37458e73dfe0c0307348
Stephen <stephen.s.espinosa@gmail.com>
2023-07-07 21:56:46 +0200

    feature add day

--- app.js
+++ app.js
@@ -5,7 +5,25 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hi");
+  const today = new Date();
+  const currentDay = today.getDay();
+  const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  if (currentDay === 6 || currentDay === 0) {
+    res.write(`<h1>Today is ${dayNames[currentDay]} and its weekend</h1>`);
+    res.send();
+  } else {
+    res.write(`<h1>Today is ${dayNames[currentDay]} and its weekday</h1>`);
+    res.send();
+  }
 });
 
 app.listen(3000, function () {
